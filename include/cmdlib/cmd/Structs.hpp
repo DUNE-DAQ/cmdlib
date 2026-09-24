7f8414df7da0046527cbb98498f133f4ed7ccc42
Marco Roda <marco.roda.88@gmail.com>
2026-09-24 12:18:25 +0100

    clang format

--- include/cmdlib/cmd/Structs.hpp
+++ include/cmdlib/cmd/Structs.hpp
@@ -1,7 +1,7 @@
 /*
  * This file is 100% generated.  Any manual edits will likely be lost.
  *
- * This contains struct and other type definitions for shema in 
+ * This contains struct and other type definitions for shema in
  * namespace dunedaq::cmdlib::cmd.
  */
 #ifndef DUNEDAQ_CMDLIB_CMD_STRUCTS_HPP
@@ -14,48 +14,48 @@
 
 namespace dunedaq::cmdlib::cmd {
 
-    // @brief The aplication name.
-    using AppId = std::string;
+// @brief The aplication name.
+using AppId = std::string;
 
-    // @brief The command name.  FIXME: this should be an enum!
-    using CmdId = std::string;
+// @brief The command name.  FIXME: this should be an enum!
+using CmdId = std::string;
 
-    // @brief An opaque object holding lower layer substructure
-    using Data = nlohmann::json;
+// @brief An opaque object holding lower layer substructure
+using Data = nlohmann::json;
 
-    // @brief Top-level command object structure
-    struct Command 
-    {
+// @brief Top-level command object structure
+struct Command
+{
 
-        // @brief Identify the type of command
-        CmdId id = "";
+  // @brief Identify the type of command
+  CmdId id = "";
 
-        // @brief Command data object with type-specific structure
-        Data data = {};
-    };
+  // @brief Command data object with type-specific structure
+  Data data = {};
+};
 
-    // @brief Outcome of the command: OK=true, NotOK=false
-    using IsOk = bool;
+// @brief Outcome of the command: OK=true, NotOK=false
+using IsOk = bool;
 
-    // @brief The command name.
-    using Result = std::string;
+// @brief The command name.
+using Result = std::string;
 
-    // @brief Top-level command reply object structure
-    struct CommandReply 
-    {
+// @brief Top-level command reply object structure
+struct CommandReply
+{
 
-        // @brief Outcome of the command: OK=true, NotOK=false
-        IsOk success = true;
+  // @brief Outcome of the command: OK=true, NotOK=false
+  IsOk success = true;
 
-        // @brief Textual result of the command
-        Result result = "";
+  // @brief Textual result of the command
+  Result result = "";
 
-        // @brief Application name
-        AppId appname = "";
+  // @brief Application name
+  AppId appname = "";
 
-        // @brief Command reply data object with type-specific structure
-        Data data = {};
-    };
+  // @brief Command reply data object with type-specific structure
+  Data data = {};
+};
 
 } // namespace dunedaq::cmdlib::cmd
 
--- test/plugins/dummyCommandFacility.cpp
+++ test/plugins/dummyCommandFacility.cpp
@@ -9,13 +9,13 @@
 #include "cmdlib/CommandFacility.hpp"
 
 #include "logging/Logging.hpp"
-#include <nlohmann/json.hpp>
 #include <cetlib/BasicPluginFactory.h>
+#include <nlohmann/json.hpp>
 
-#include <thread>
 #include <chrono>
 #include <memory>
 #include <string>
+#include <thread>
 
 using namespace dunedaq::cmdlib;
 using namespace std::chrono_literals;
@@ -23,20 +23,23 @@ using namespace std::chrono_literals;
 class dummyCommandFacility : public CommandFacility
 {
 public:
-  explicit dummyCommandFacility(std::string uri, int n) :
-  CommandFacility(uri),
-  n_command{n} {
+  explicit dummyCommandFacility(std::string uri, int n)
+    : CommandFacility(uri)
+    , n_command{ n }
+  {
   }
 
-  void run(std::atomic<bool>& end_marker) {
+  void run(std::atomic<bool>& end_marker)
+  {
     TLOG() << "Going for a run... " << n_command;
 
     bool once = true;
     while (end_marker) {
       if (once) {
         // execute n_command quick commands
-        for (auto i=0; i<n_command; ++i) {
-          auto democmd = nlohmann::json::parse("{\"happy\": true, \"pi\": 3.141, \"count\": " + std::to_string(i) + "}");
+        for (auto i = 0; i < n_command; ++i) {
+          auto democmd =
+            nlohmann::json::parse("{\"happy\": true, \"pi\": 3.141, \"count\": " + std::to_string(i) + "}");
           inherited::execute_command(democmd, cmd::CommandReply());
         }
 
@@ -45,29 +48,32 @@ public:
         inherited::execute_command(slowcmd, cmd::CommandReply());
 
         // execute again n_command quick command
-        for (auto i=0; i<n_command; ++i) {
-          auto democmd = nlohmann::json::parse("{\"happy\": true, \"pi\": 3.141, \"count\": " + std::to_string(i) + "}");
+        for (auto i = 0; i < n_command; ++i) {
+          auto democmd =
+            nlohmann::json::parse("{\"happy\": true, \"pi\": 3.141, \"count\": " + std::to_string(i) + "}");
           inherited::execute_command(democmd, cmd::CommandReply());
         }
         once = false;
       }
     }
 
-    TLOG_DEBUG(1) <<"Finished.";
+    TLOG_DEBUG(1) << "Finished.";
   }
 
 protected:
   typedef CommandFacility inherited;
   int n_command;
 
-  void completion_callback(const cmdobj_t& cmd, cmd::CommandReply& meta) {
+  void completion_callback(const cmdobj_t& cmd, cmd::CommandReply& meta)
+  {
     TLOG() << "Command " << cmd << "\nexecution resulted with: " << meta.result;
   }
-
 };
 
-extern "C" {
-  std::shared_ptr<dunedaq::cmdlib::CommandFacility> make(std::string uri, int n) {
+extern "C"
+{
+  std::shared_ptr<dunedaq::cmdlib::CommandFacility> make(std::string uri, int n)
+  {
     return std::shared_ptr<dunedaq::cmdlib::CommandFacility>(new dummyCommandFacility(uri, n));
   }
 }
